0c393b39979ec2df1b96d35deb10ddc231085bfb
Twin Ali <scientists.twin@gmail.com>
2022-03-24 01:39:46 +0400

    feature: add autoLoadEntities property to main

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 import validationSchema from './config/configs.schema';
 
 @Module({
   imports: [
     UserModule,
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
       validationSchema: validationSchema,
     }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
