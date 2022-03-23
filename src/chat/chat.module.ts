afade71481da6d3a9e441636c6b1c2729238ef6f
Twin Ali <scientists.twin@gmail.com>
2022-03-24 01:59:02 +0400

    feature: add chatGateway to chatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatGateway } from './chat.gateway';
 import { MessageRepository } from './repository/message.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageRepository])],
+  providers: [ChatGateway],
 })
 export class ChatModule {}
