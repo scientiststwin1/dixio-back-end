c028f1d66db1a2a98c57fa672ff8ef366b37d8b0
Twin Ali <scientists.twin@gmail.com>
2022-03-24 11:50:19 +0400

    feature: add repository and gataway and service to module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatGateway } from './chat.gateway';
 import { MessageRepository } from './repository/message.repository';
+import { ChatService } from './chat.service';
+import { UserRepository } from 'src/user/repository/user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessageRepository])],
-  providers: [ChatGateway],
+  imports: [TypeOrmModule.forFeature([MessageRepository, UserRepository])],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
